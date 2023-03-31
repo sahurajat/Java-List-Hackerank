13a361ae372d1e20a5a2db9a9ad0582a6c938f9f
Rajat Sahu <119427607+sahurajat@users.noreply.github.com>
2023-03-31 22:31:32 +0530

    datatypes-hackerrank-problem

---
+++ datatypes.java
@@ -0,0 +1,47 @@
+//Java has 8 primitive data types; char, boolean, byte, short, int, long, float, and double. For this exercise, we'll work with the primitives used to hold integer values (byte, short, int, and long):
+
+// A byte is an 8-bit signed integer.
+// A short is a 16-bit signed integer.
+// An int is a 32-bit signed integer.
+// A long is a 64-bit signed integer.
+// Given an input integer, you must determine which primitive data types are capable of properly storing that input.
+
+// To get you started, a portion of the solution is provided for you in the editor.
+import java.util.*;
+import java.io.*;
+
+
+
+class datatypes{
+    public static void main(String []argh)
+    {
+
+
+
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+
+        for(int i=0;i<t;i++)
+        {
+
+            try
+            {long x=sc.nextLong();
+                System.out.println(x+" can be fitted in:");
+                if(x>=Byte.MIN_VALUE && x<=Byte.MAX_VALUE)
+                    System.out.println("* byte");
+                if(x>=Short.MIN_VALUE && x<=Short.MAX_VALUE)
+                    System.out.println("* short");
+                if(x>=Integer.MIN_VALUE && x<=Integer.MAX_VALUE)
+                    System.out.println("* int");
+                if(x>=Long.MIN_VALUE && x<=Long.MAX_VALUE)
+                    System.out.println("* long");
+            }
+            catch(Exception e)
+            {
+                System.out.println(sc.next()+" can't be fitted anywhere.");
+            }
+
+        }
+    }
+}
+
